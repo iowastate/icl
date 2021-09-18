1642aa7da67b5dfe8b16c38c5c1f8b0a542fa419
obrien-isu <89813798+obrien-isu@users.noreply.github.com>
2021-09-17 22:10:34 -0500

    Update bfs.java

--- graphs/bfs.java
+++ graphs/bfs.java
@@ -2,18 +2,18 @@
 *   @author obrien
 */
 void bfs(ArrayList<Node> graph, Node start) {
-		start.visited = true;
-		Queue<Node> q = new LinkedList<>();
-		q.add(start);
+	start.visited = true;
+	Queue<Node> q = new LinkedList<>();
+	q.add(start);
 
-		while (!q.isEmpty()) {
-				Node u = q.poll();
-				for (int i = 0; i < u.neighbors.size(); i++) {
-					if (!u.neighbors.get(i).visited) {
-						u.neighbors.get(i).visited = true;
-						q.add(u.neighbors.get(i));
-					}
+	while (!q.isEmpty()) {
+			Node u = q.poll();
+			for (int i = 0; i < u.neighbors.size(); i++) {
+				if (!u.neighbors.get(i).visited) {
+					u.neighbors.get(i).visited = true;
+					q.add(u.neighbors.get(i));
 				}
 			}
 		}
 	}
+}
