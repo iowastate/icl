760ff23374a1cc231443023c5a51f3c6a779c755
obrien-isu <89813798+obrien-isu@users.noreply.github.com>
2021-09-17 22:13:09 -0500

    Update bfs.java

--- graphs/bfs.java
+++ graphs/bfs.java
@@ -17,3 +17,33 @@ void bfs(ArrayList<Node> graph, Node start) {
 		}
 	}
 }
+
+/** counting depth of breathFirstSearch
+*   @obrien
+*/
+int breadthFirstSearch(ArrayList<Node> graph, Node start) {
+	start.visited = true;
+	Queue<Node> q = new LinkedList<>();
+	q.add(start);
+	int depth = 1;
+	int depthInc = 0;
+	int depthCounter = 0;
+
+	while (!q.isEmpty()) {
+		while (depth > 0) {
+			Node u = q.poll();
+			depth--;
+			for (int i = 0; i < u.neighbors.size(); i++) {
+				if (!u.neighbors.get(i).visited) {
+					u.neighbors.get(i).visited = true;
+					q.add(u.neighbors.get(i));
+					depthInc++;
+				}
+			}
+		}
+		depth = depthInc;
+		depthInc = 0;
+		depthCounter++;
+	}
+	return depthCounter;
+}
