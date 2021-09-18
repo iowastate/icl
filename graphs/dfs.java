4bad7776aef068d5a113f9ce735352222f80020c
obrien-isu <89813798+obrien-isu@users.noreply.github.com>
2021-09-17 22:04:57 -0500

    Update dfs.java

--- graphs/dfs.java
+++ graphs/dfs.java
@@ -2,30 +2,30 @@
 *   @author obrien
 */ 
 void dfs(ArrayList<Node> graph, Node start) {
-		start.visited = true;
-		Stack<Node> stk = new Stack<>();
-		stk.push(start);
+	start.visited = true;
+	Stack<Node> stk = new Stack<>();
+	stk.push(start);
 
-		while (!stk.isEmpty()) {
-			Node u = stk.pop();
-			for (int i = 0; i < u.neighbors.size(); i++) {
-				if (!u.neighbors.get(i).visited) {
-					u.neighbors.get(i).visited = true;
-					stk.push(u.neighbors.get(i));
-				}
+	while (!stk.isEmpty()) {
+		Node u = stk.pop();
+		for (int i = 0; i < u.neighbors.size(); i++) {
+			if (!u.neighbors.get(i).visited) {
+				u.neighbors.get(i).visited = true;
+				stk.push(u.neighbors.get(i));
 			}
 		}
 	}
+}
 
 /** depthFirstSearch - recursive
 *   @author obrien
 */
 void dfs(Node start) {
-		start.visited = true;
-		s.add(start.data);
-		for (int i = 0; i < start.neighbors.size(); i++) {
-			if (!start.neighbors.get(i).visited) {
-				dfs(start.neighbors.get(i));
-			}
+	start.visited = true;
+	s.add(start.data);
+	for (int i = 0; i < start.neighbors.size(); i++) {
+		if (!start.neighbors.get(i).visited) {
+			dfs(start.neighbors.get(i));
 		}
 	}
+}
